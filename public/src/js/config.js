d4c07e9210d04a751d5e10123ef35d02679ca867
Gametoshi <44985491+Globaleliteee@users.noreply.github.com>
2018-12-17 20:58:56 +0100

    Update for Gamecredits

--- public/src/js/config.js
+++ public/src/js/config.js
@@ -5,7 +5,7 @@ angular.module('insight').config(function($routeProvider) {
   $routeProvider.
     when('/block/:blockHash', {
       templateUrl: 'views/block.html',
-      title: 'Litecoin Block '
+      title: 'Gamecredits Block '
     }).
     when('/block-index/:blockHeight', {
       controller: 'BlocksController',
@@ -17,7 +17,7 @@ angular.module('insight').config(function($routeProvider) {
     }).
     when('/tx/:txId/:v_type?/:v_index?', {
       templateUrl: 'views/transaction.html',
-      title: 'Litecoin Transaction '
+      title: 'Gamecredits Transaction '
     }).
     when('/', {
       templateUrl: 'views/index.html',
@@ -25,15 +25,15 @@ angular.module('insight').config(function($routeProvider) {
     }).
     when('/blocks', {
       templateUrl: 'views/block_list.html',
-      title: 'Litecoin Blocks solved Today'
+      title: 'Gamecredits Blocks solved Today'
     }).
     when('/blocks-date/:blockDate/:startTimestamp?', {
       templateUrl: 'views/block_list.html',
-      title: 'Litecoin Blocks solved '
+      title: 'Gamecredits Blocks solved '
     }).
     when('/address/:addrStr', {
       templateUrl: 'views/address.html',
-      title: 'Litecoin Address '
+      title: 'Gamecredits Address '
     }).
     when('/status', {
       templateUrl: 'views/status.html',
